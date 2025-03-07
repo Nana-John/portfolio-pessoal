f157a77e470061f6f873d203ba611d8158a584ba
Alessandra Mairis <159563760+Nana-John@users.noreply.github.com>
2025-03-06 21:41:22 -0300

    Delete src directory

--- src/styles/reducers/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import someReducer from './someReducer' // Caminho correto para o 'someReducer'
-
-const rootReducer = combineReducers({
-  someState: someReducer,
-})
-
-export default rootReducer
--- src/theme.ts
+++
@@ -1,9 +0,0 @@
-export const theme = {
-  colors: {
-    primary: '#0F103F', // Exemplo de cor
-    secondary: '#1C1C65',
-    accent: '#2F2F8A',
-    highlight: '#C961DE',
-    info: '#2954A3', // Agora a propriedade 'info' pode ser usada
-  },
-}
